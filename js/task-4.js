231cb31a70f001d60c11bb3dc05a26eae41db2d0
EZH1809 <stellage.ua@gmail.com>
2024-01-19 09:13:55 +0200

    add

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
 'use strict'
+// const myArray = [1, 2, 3];
+// // for (let i = 0; i < myArray.length; i++) {
+// //     console.log(myArray[i])
+// // }
+// myArray.forEach((element, index) => {
+//     console.log(element, index);
+// })
+
+// const userProfile = {
+//     name: 'Bogdan',
+//     commentsQty: 23,
+//     hasSignedAgreement: false
+// }
+// const userInfo = ({ name, commentsQty }) => {
+//     if (!commentsQty) {
+//         return `User ${name} has no comments`
+//     }
+//     return `User ${name} has ${commentsQty} comments`
+// }
+// userInfo(userProfile)
+
+const words = ["JavaScript", "is", "amazing"];
+console.log(words.join("")); // 'JavaScriptisamazing'
+console.log(words.join(" ")); // 'JavaScript is amazing'
+console.log(words.join("-")); // 'JavaScript-is-amazing'
